9be8282cb95e77a00457c1f25fda1253b99424e5
酒枫子 <maple_leaf_7@msn.com>
2016-01-30 14:08:06 +0800

    定位

--- src/android/LocationInfo.java
+++ src/android/LocationInfo.java
@@ -1,24 +1,92 @@
 package com.tyrion.plugin.location;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.location.AMapLocationClientOption.AMapLocationMode;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * This class echoes a string called from JavaScript.
  */
-public class LocationInfo extends CordovaPlugin {
+public class LocationInfo extends CordovaPlugin implements AMapLocationListener{
+
+    private AMapLocationClient locationClient = null;
+    private AMapLocationClientOption locationOption = null;
+    CallbackContext callback;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("get")) {
+            callback = callbackContext;
             String message = args.getString(0);
+
+            locationClient = new AMapLocationClient(this.cordova.getActivity().getApplicationContext());
+            locationOption = new AMapLocationClientOption();
+            // 设置定位模式为低功耗模式
+            locationOption.setLocationMode(AMapLocationMode.Hight_Accuracy);
+            //设置为单次定位
+            locationOption.setOnceLocation(true);
+            // 设置定位监听
+            locationClient.setLocationListener(this);
+            locationOption.setNeedAddress(true);
+            locationOption.setInterval(2000);
+
+            locationClient.setLocationOption(locationOption);
+            // 启动定位
+            locationClient.startLocation();
 
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(aMapLocation.getTime());
+                df.format(date);//定位时间
+
+                JSONObject locationInfo = new JSONObject();
+                try {
+                    locationInfo.put("type", aMapLocation.getLocationType()); //获取当前定位结果来源，如网络定位结果，详见定位类型表
+                    locationInfo.put("latitude", aMapLocation.getLatitude()); //获取纬度
+                    locationInfo.put("longitude", aMapLocation.getLongitude()); //获取经度
+                    locationInfo.put("accuracy", aMapLocation.getAccuracy()); //获取精度信息
+                    locationInfo.put("date", date); //定位时间
+                    locationInfo.put("address", aMapLocation.getAddress()); //地址，如果option中设置isNeedAddress为false，则没有此结果
+                    locationInfo.put("country", aMapLocation.getCountry()); //国家信息
+                    locationInfo.put("province", aMapLocation.getProvince()); //省信息
+                    locationInfo.put("city", aMapLocation.getCity()); //城市信息
+                    locationInfo.put("district", aMapLocation.getDistrict()); //城区信息
+                    locationInfo.put("road", aMapLocation.getRoad()); //街道信息
+                    locationInfo.put("cityCode", aMapLocation.getCityCode()); //城市编码
+                    locationInfo.put("adCode", aMapLocation.getAdCode()); //地区编码
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+//                Log.e("locationInfo", locationInfo.toString());
+                PluginResult result = new PluginResult(PluginResult.Status.OK, locationInfo);
+                callback.sendPluginResult(result);
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+//                Log.e("ErrorCode",aMapLocation.getErrorCode()+"");
+                PluginResult result = new PluginResult(PluginResult.Status.ERROR, aMapLocation.getErrorCode());
+                callback.sendPluginResult(result);
+            }
+        }
     }
 }
