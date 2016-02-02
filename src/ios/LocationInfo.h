75113eb8351f865dd80886622e872813c31fbbb1
zsly <zsly3n@163.com>
2016-02-02 11:50:55 +0800

    add

---
+++ src/ios/LocationInfo.h
@@ -0,0 +1,13 @@
+//
+//  LocationInfo.h
+//  泛艺术
+//
+//  Created by zsly on 16/2/2.
+//
+//
+
+#import <Cordova/CDV.h>
+
+@interface LocationInfo : CDVPlugin
+-(void)get:(CDVInvokedUrlCommand*)command;
+@end
--- src/ios/LocationInfo.m
+++ src/ios/LocationInfo.m
@@ -1,28 +1,112 @@
-/********* LocationInfo.m Cordova Plugin Implementation *******/
+//
+//  LocationInfo.m
+//  泛艺术
+//
+//  Created by zsly on 16/2/2.
+//
+//
 
-#import <Cordova/CDV.h>
+#import "LocationInfo.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface LocationInfo : CDVPlugin {
-  // Member variables go here.
-}
-
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@interface LocationInfo()<CLLocationManagerDelegate>
+@property (nonatomic,strong) CLLocationManager* locationManager;
+@property (nonatomic,strong) NSString*callbackId;
 @end
 
+
 @implementation LocationInfo
-
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
+-(void)get:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
+    self.callbackId=command.callbackId;
+    [self checkAuthority];
+}
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+-(void)checkAuthority
+{
+    if([CLLocationManager locationServicesEnabled])
+    {
+        if (!self.locationManager)
+        {
+            self.locationManager = [[CLLocationManager alloc] init];
+        }
+        _locationManager.delegate           = self;
+        _locationManager.distanceFilter     = 500;
+        _locationManager.desiredAccuracy    = kCLLocationAccuracyBestForNavigation;
+        if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0)
+        {
+            [_locationManager requestWhenInUseAuthorization];
+            // 开始时时定位
+        }
+        [_locationManager startUpdatingLocation];
     }
+    else if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusDenied)
+    {
+        [self handleFailedMsg:@"定位服务未开启"];
+    }
+}
 
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    switch (status) {
+        case kCLAuthorizationStatusNotDetermined:
+            if ([_locationManager respondsToSelector:@selector(requestAlwaysAuthorization)])
+            {
+                [_locationManager requestWhenInUseAuthorization];
+            }
+            break;
+        case kCLAuthorizationStatusDenied:
+        {
+            
+        }
+        default:
+            break;
+    }
+}
+
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    // NSLog(@"error:%@",error);
+    [self handleFailedMsg:@"定位服务未开启"];
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    
+    // NSLog(@"%ld", (unsigned long)[locations count]);
+    
+    CLLocation *newLocation             = locations[0];
+    //    CLLocationCoordinate2D coordinate   = newLocation.coordinate;
+    //    NSLog(@"经度：%f,纬度：%f",coordinate.longitude,coordinate.latitude);
+    
+    [manager stopUpdatingLocation];
+    
+    //------------------位置反编码---5.0之后使用-----------------
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [geocoder reverseGeocodeLocation:newLocation
+                   completionHandler:^(NSArray *placemarks, NSError *error){
+                       if(error)
+                       {
+                           NSString *error_msg=[NSString stringWithFormat:@"定位解析失败:%@",error.description];
+                           [self handleFailedMsg:error_msg];
+                       }
+                       else
+                       {
+                           for (CLPlacemark *place in placemarks) {
+                               NSString* province      = place.administrativeArea;
+                               NSString* city         = place.locality;
+                               NSString* district         = place.subLocality;
+                               CDVPluginResult*pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{@"province":province,@"city":city,@"district":district}];
+                               [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
+                           }
+                       }
+                   }];
+}
+
+
+
+-(void)handleFailedMsg:(NSString*)msg
+{
+     CDVPluginResult*pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:msg];
+     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
 }
 
 @end
